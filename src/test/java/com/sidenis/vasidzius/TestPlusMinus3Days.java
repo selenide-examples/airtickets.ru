ba2edd09339ad307ec81c4b7b939a96574e3b659
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-05-11 20:13:27 +0300

    add test for selecting "+/- 3 days" button

---
+++ src/test/java/com/sidenis/vasidzius/TestPlusMinus3Days.java
@@ -0,0 +1,31 @@
+package com.sidenis.vasidzius;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.selected;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestPlusMinus3Days {
+  @Before
+  public void setUp() {
+    Configuration.browser = "chrome";
+    open("http://www.airtickets.ru/");
+  }
+
+  @Test
+  public void canSelectRadioByClass() {
+    $(".plusMinus3DaysRadioButton .radioButtonContainer", 1).click();
+    $(By.name("plusMinus3")).shouldBe(selected);
+  }
+
+  @Test
+  public void canSelectRadioByText() {
+    $(".plusMinus3DaysRadioButton").find(byText("+/- 3 дня")).click();
+    $(By.name("plusMinus3")).shouldBe(selected);
+  }
+}
